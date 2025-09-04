41f5b48fc2f4142a15257002e956cd0b26f87259
jbmll <juniorbmorales@gmail.com>
2025-09-04 12:52:14 -0500

    feat: agregar DTOs ClienteResponseDto y DireccionResponseDto para gestión de clientes y direcciones

---
+++ src/main/java/com/minegocio/facturacion/dto/ClienteResponseDto.java
@@ -0,0 +1,44 @@
+package com.minegocio.facturacion.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.LocalDateTime;
+
+/**
+ * CASO: Funcionalidad para buscar y obtener un listado de clientes
+ * DTO de respuesta que incluye la dirección matriz
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ClienteResponseDto {
+    
+    private Long id;
+    
+    @JsonProperty("tipo_identificacion")
+    private String tipoIdentificacion;
+    
+    @JsonProperty("numero_identificacion")
+    private String numeroIdentificacion;
+    
+    private String nombres;
+    
+    private String correo;
+    
+    @JsonProperty("numero_celular")
+    private String numeroCelular;
+    
+    @JsonProperty("direccion_matriz")
+    private DireccionResponseDto direccionMatriz;
+    
+    @JsonProperty("fecha_creacion")
+    private LocalDateTime fechaCreacion;
+    
+    @JsonProperty("fecha_actualizacion")
+    private LocalDateTime fechaActualizacion;
+}
---
+++ src/main/java/com/minegocio/facturacion/dto/DireccionResponseDto.java
@@ -0,0 +1,37 @@
+package com.minegocio.facturacion.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * CASO: Listar direcciones, Respuesta con dirección matriz
+ * DTO de respuesta para direcciones
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DireccionResponseDto {
+    
+    private Long id;
+    
+    private String provincia;
+    
+    private String ciudad;
+    
+    private String direccion;
+    
+    @JsonProperty("es_matriz")
+    private Boolean esMatriz;
+    
+    @JsonProperty("fecha_creacion")
+    private LocalDateTime fechaCreacion;
+    
+    @JsonProperty("fecha_actualizacion")
+    private LocalDateTime fechaActualizacion;
+}
